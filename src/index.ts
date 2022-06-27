64199f6a6e2e56237e8637e932fc92ee7cbc53a6
britolmbs <britolmbs@hotmail.com>
2022-06-27 15:56:30 -0300

    desenvolvendo metodo Post

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,18 @@ import usersRoute from "./routes/users.routes";
 
 const app = express();
 
+// Configuração da Aplicação
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+// Configuração de Rotas
 app.use(usersRoute);
 
 app.get('/status', (req: Request, res: Response, next: NextFunction) => {
 res.status(200).send({ foo: 'Sucesso!' });
 });
 
+//Inicialização do servidor
 app.listen(3000, () => {
     console.log('Aplicação executando na porta 3000!');
 });
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,16 +1,22 @@
 import { Router, Request, Response, NextFunction } from 'express';
-import { request } from 'http';
+import { StatusCodes } from 'http-status-codes';
 
 const usersRoute = Router();
 
 usersRoute.get('/users', (req: Request, res: Response, next: NextFunction) => {
 const users = [{ userName: 'Lucas'}];
-res.status(200).send({users});
+res.status(StatusCodes.OK).send({users});
 });
 
 usersRoute.get('/users/:uuid', (req: Request<{uuid: string }>, res: Response, next: NextFunction)=> {
    const uuid =  req.params.uuid;
-   res.status(200).send({ uuid }); 
+   res.status(StatusCodes.OK).send({ uuid }); 
+});
+
+usersRoute.post('/users', (req: Request, res: Response, next: NextFunction) => {
+   const newUser = req.body;
+
+   res.status(StatusCodes.CREATED).send(newUser);
 });
 
 export default usersRoute;
