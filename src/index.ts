b47c1ed83ffea79d50f5984c2bd122cf0002d3d4
britolmbs <britolmbs@hotmail.com>
2022-06-27 11:43:54 -0300

    Automatizando o servidor para identificar alterações no codigo

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,9 @@ import express, { Request, Response, NextFunction } from "express";
 const app = express();
 
 app.get('/status', (req: Request, res: Response, next: NextFunction) => {
-res.status(200).send({ foo: 'bar' });
+res.status(200).send({ foo: 'Sucesso total!' });
 });
 
 app.listen(3000, () => {
     console.log('Aplicação executando na porta 3000!');
-})
+});
