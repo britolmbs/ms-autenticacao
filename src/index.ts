ea9af0b2c5ac0939255484baf8283641b5dc3fea
britolmbs <britolmbs@hotmail.com>
2022-06-28 10:35:07 -0300

    Projeto Finalizado

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,17 @@
-import express, { Request, Response, NextFunction } from "express";
+import express from "express";
+import statusRoute from "./routes/status.route";
 import usersRoute from "./routes/users.routes";
 
 const app = express();
 
 // Configuração da Aplicação
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 // Configuração de Rotas
 app.use(usersRoute);
 
-app.get('/status', (req: Request, res: Response, next: NextFunction) => {
-res.status(200).send({ foo: 'Sucesso!' });
-});
-
+app.use(statusRoute)
 //Inicialização do servidor
 app.listen(3000, () => {
     console.log('Aplicação executando na porta 3000!');
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,16 +1,16 @@
-import { Router, Request, Response, NextFunction } from 'express';
+import { NextFunction, Request, Response, Router } from 'express';
 import { StatusCodes } from 'http-status-codes';
 
 const usersRoute = Router();
 
 usersRoute.get('/users', (req: Request, res: Response, next: NextFunction) => {
-const users = [{ userName: 'Lucas'}];
-res.status(StatusCodes.OK).send({users});
+   const users = [{ userName: 'Lucas' }];
+   res.status(StatusCodes.OK).send({ users });
 });
 
-usersRoute.get('/users/:uuid', (req: Request<{uuid: string }>, res: Response, next: NextFunction)=> {
-   const uuid =  req.params.uuid;
-   res.status(StatusCodes.OK).send({ uuid }); 
+usersRoute.get('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+   const uuid = req.params.uuid;
+   res.status(StatusCodes.OK).send({ uuid });
 });
 
 usersRoute.post('/users', (req: Request, res: Response, next: NextFunction) => {
@@ -20,15 +20,15 @@ usersRoute.post('/users', (req: Request, res: Response, next: NextFunction) => {
    res.status(StatusCodes.CREATED).send(newUser);
 });
 
-usersRoute.put('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) =>{
-const uuid = req.params.uuid;
-const modifiedUser = req.body;
+usersRoute.put('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+   const uuid = req.params.uuid;
+   const modifiedUser = req.body;
 
-modifiedUser.uuid = uuid;
-res.status(StatusCodes.OK).send(modifiedUser);
+   modifiedUser.uuid = uuid;
+   res.status(StatusCodes.OK).send(modifiedUser);
 });
 
-usersRoute.delete('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction ) =>{
+usersRoute.delete('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
    res.sendStatus(StatusCodes.OK);
 });
 
