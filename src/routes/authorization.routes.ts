322b4c268f94c492eea82156bba401fe8d8e32f4
britolmbs <britolmbs@hotmail.com>
2022-07-01 15:31:46 -0300

    Middleware para reutilizar as funções de autenticação

--- src/routes/authorization.routes.ts
+++ src/routes/authorization.routes.ts
@@ -3,36 +3,18 @@ import ForbiddenError from "../models/errors/forbidden.error.model";
 import userRepository from "../repositories/user.repository";
 import JWT from 'jsonwebtoken';
 import { StatusCodes } from "http-status-codes";
+import basicAuthenticationMiddleware from "../middlewares/basic-authentication.middleware";
 
 const authorizationRoute = Router();
 
-authorizationRoute.post('/token', async (req: Request, res: Response, next: NextFunction) => {
+authorizationRoute.post('/token', basicAuthenticationMiddleware , async (req: Request, res: Response, next: NextFunction) => {
 
     try {
-        const authorizationHeader = req.headers['authorization'];
+        const user = req.user;
 
-        if (!authorizationHeader) {
-            throw new ForbiddenError('Credenciais não informadas ');
+        if(!user){
+            throw new ForbiddenError('Usuario não informado!')
         }
-
-        const [authenticationType, token] = authorizationHeader.split(' ');
-
-        if (authenticationType !== 'Basic' || !token) {
-            throw new ForbiddenError('Tipo de autenticação Invalido ');
-        }
-
-        const tokenContent = Buffer.from(token, 'base64').toString('utf-8')
-
-        const [username, password] = tokenContent.split(':');
-
-        if (!username || !password) {
-            throw new ForbiddenError('crendecias não preenchidas ');
-        }
-
-        const user = await userRepository.findByUsernameAndPassword(username, password);
-       if(!user) {
-        throw new ForbiddenError('Usuario ou senha invalidos!'); }
-
         const jwtPlayLoad = { username: user.username};
         const jwtOptions = { subject: user?.uuid};
         const secretKey = 'my_secret_key';
